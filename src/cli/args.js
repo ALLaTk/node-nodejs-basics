de219e4d411a2e94b5639ca7f3e758c887ef4912
Alla <regicter1810@mail.ru>
2022-12-04 22:22:36 +0300

    feat: implement сommand line interface(src/cli)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
+import { argv } from 'process';
 const parseArgs = () => {
-    // Write your code here 
+  const argvArr = argv
+  const arr = [];
+  for (let i = 0; i < argvArr.length; i++) {
+    if (argvArr[i].startsWith('--')) {
+      arr.push(`${argvArr[i].slice(2)} is ${argvArr[i+1]}`);
+    }
+  }
+  console.log(arr.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const arr = [];
+  for (let key in process.env) {
+    if(key.startsWith('RSS_')){
+      arr.push(`${key}=${process.env[key]}`);
+    }
+  }
+  console.log(arr.join('; '));
 };
 
 parseEnv();
