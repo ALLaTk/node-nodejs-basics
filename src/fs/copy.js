de8adbf69e50a8233bc0ff854f8e0dff2a42d6b7
Alla <regicter1810@mail.ru>
2022-12-12 18:37:07 +0300

    feat: implement file system (src/fs)

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,43 @@
-const copy = async () => {
-    // Write your code here 
-};
+import fs from 'fs';
+import promise from 'fs/promises';
+const pathFile = './files'
+const pathFileCopy = './files_copy';
 
-await copy();
+export const copy = async (pathFile, pathFileCopy) => {
+
+  try {
+  
+    await promise.mkdir(pathFileCopy);
+  
+    fs.readdir(pathFileCopy, (err, data) => {
+      if (err) throw err;
+      for (let elem of data) {
+        fs.access(`./${pathFile}/${elem}`, (err) => {
+          if (err) {
+            fs.rm(`./${pathFileCopy}/${elem}`, (err) => {
+              if (err) throw err;
+            });
+          }
+        });
+      }
+    });
+  
+    let data = await promise.readdir(pathFile, { withFileTypes: true })
+     
+    for (let el of data) {
+      if (el.isFile()) {
+        fs.copyFile(`./${pathFile}/${el.name}`, `./${pathFileCopy}/${el.name}`, (err) => {
+          if (err) throw err;
+        });
+      }
+      else if (el.isDirectory()) {
+        copy(`./${pathFile}/${el.name}`, `./${pathFileCopy}/${el.name}`);
+      }
+    }
+  
+  } catch(err){
+      console.error(new Error('FS operation failed'));
+  }
+}
+
+await copy(pathFile, pathFileCopy);
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fs from 'fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const file = fileURLToPath(import.meta.url)
+const dir = dirname(file)
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.writeFile(dir + '/files/fresh.txt', 'I am fresh and young', { flag: 'wx'})
+  } catch (error) {
+    console.error(new Error('FS operation failed'))
+  }
 };
 
 await create();
